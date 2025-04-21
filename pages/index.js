2b982909f0288be6f84fa68988454e97a2339c8b
DiavoloCOBRETY <dalvarezp@hotmail.com>
2025-04-21 22:58:33 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
-export default function Registro() {
+import Link from "next/link";
+
+export default function Home() {
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
-      <h2>Registro</h2>
-      <p>Aquí irá el formulario de inscripción.</p>
+      <h1>Bienvenido a La Búsqueda</h1>
+      <Link href="/registro">
+        <button style={{ margin: "10px" }}>Registro</button>
+      </Link>
+      <Link href="/matrix">
+        <button style={{ margin: "10px" }}>Ir al reto Matrix</button>
+      </Link>
     </div>
   );
 }
