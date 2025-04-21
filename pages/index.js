a422c25ad1a846f699679922172b5ba146912f5f
DiavoloCOBRETY <dalvarezp@hotmail.com>
2025-04-21 22:56:43 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-
+export default function Registro() {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
+      <h2>Registro</h2>
+      <p>Aquí irá el formulario de inscripción.</p>
+    </div>
+  );
+}
